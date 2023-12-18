91e87215282e262118255ac067ffc17836efe634
Caroline Jaffré <120339746+carolinejffr@users.noreply.github.com>
2023-12-18 17:04:20 +0100

    update partie frontend

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE HTML>
 <html>
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+<header>
+	<h1>TP API - Caroline Jaffré</h1>
+	<a href="https://github.com/carolinejffr/API-PHP-C">Voir sur Github</a>
+</header>
 <body>
 	<form method="post">
 		<label for="commande">Entrez ici la commande à exécuter ou help pour la liste des commandes :</label><br />
 		<input type="text" id="commande" name="commande">
 		<input type="submit" value="Envoyer">
 	</form>
+	<br />
 <?php	
 if (isset($_POST["commande"]))
 {
